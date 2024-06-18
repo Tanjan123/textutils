ecad3b01497e1cc9920a1143042ab5548d2b01a3
Tanjan Singh Karki <166477216+Tanjan123@users.noreply.github.com>
2024-06-18 19:27:51 +0545

    Updates

---
+++ precache-manifest.842ef24719277bd42c5312895f2c1600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca4f4789900c8d14aca978fbde1920f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b1a73487de70d285b30d",
+    "url": "/my-app/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "c563c72d9ac5e6774abe",
+    "url": "/my-app/static/js/2.aef00dbc.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/my-app/static/js/2.aef00dbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82594277ffdbbbbe6c96",
+    "url": "/my-app/static/js/3.f00e9902.chunk.js"
+  },
+  {
+    "revision": "b1a73487de70d285b30d",
+    "url": "/my-app/static/js/main.50342e2e.chunk.js"
+  },
+  {
+    "revision": "5549b0f24f1d0a4d74f6",
+    "url": "/my-app/static/js/runtime-main.48e1deef.js"
+  }
+]);
